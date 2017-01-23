9e56d67a804dfed931625538c78a2032c35067a4
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2017-01-23 16:24:46 -0600

    empty love2d game

---
+++ conf.lua
@@ -0,0 +1,19 @@
+-- Configuration
+function love.conf(game)
+    game.title = "GAME_NAME_PRETTY"
+    game.identity = "GAME_NAME_IDSTR"
+    game.version = "0.10.2"
+    game.window.width = 480
+    game.window.height = 800
+    game.window.resizable = false
+    game.window.fullscreen = false
+    game.window.vsync = false
+
+    -- debugging
+    game.console = true
+
+    -- we (probably) won't need physics
+    game.modules.physics = false
+
+    -- game.window.icon =
+end
