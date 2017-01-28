046e629fda526f5ec98712c96a76f9c703d3a8a3
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2017-01-28 14:58:59 -0600

    forgot a file

---
+++ colors.lua
@@ -0,0 +1,21 @@
+colors = {
+    invis = {0, 0, 0, 0},
+    default = {255, 255, 255, 255},
+
+    gridlines = {255, 255, 255, 220},
+    grid_bg = {0, 0, 0, 130},
+    grid_sel = {184, 184, 0, 184},
+
+    tile_border = {40, 40, 40, 220},
+    tile_bg = {190, 190, 190, 210},
+
+    pipe_inside = {160, 160, 160, 255},
+    pipe_outside = {80, 80, 80, 255},
+
+    drain_lines = {40, 40, 40, 255},
+    drain_holes = {70, 70, 70, 180},
+    drain_bg = {215, 215, 215, 255},
+
+    faucet_lines = {40, 40, 40, 255},
+    faucet_fg = {46, 59, 173, 255},
+}
